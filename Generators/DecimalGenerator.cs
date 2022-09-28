764bc0c0b4f4a4bf7e022ec91f7fadd1227bce02
KJI0YH <aliaksei.kryzhanouski@gmail.com>
2022-09-28 22:29:16 +0300

    add decimal generator

---
+++ Generators/DecimalGenerator.cs
@@ -0,0 +1,24 @@
+﻿using Core.Context;
+using Core.Interfaces;
+
+namespace Core.Generators
+{
+    public class DecimalGenerator : IValueGenerator
+    {
+        public bool CanGenerate(Type type)
+        {
+            return type == typeof(decimal);
+        }
+
+        public object Generate(Type typeToGenerate, GeneratorContext context)
+        {
+            int lo = context.Random.Next(0, int.MaxValue);
+            int mid = context.Random.Next(0, int.MaxValue);
+            int hi = context.Random.Next(0, int.MaxValue);
+            bool isNegative = context.Random.Next(0, 2) == 1;
+            byte scale = (byte)context.Random.Next(0, 29);
+            return new decimal(lo, mid, hi, isNegative, scale);
+
+        }
+    }
+}
