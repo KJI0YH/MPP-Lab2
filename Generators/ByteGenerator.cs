d2cb2d3f2e696637e46c6a1ac36c91e28d0ee488
KJI0YH <aliaksei.kryzhanouski@gmail.com>
2022-09-28 22:22:23 +0300

    add byte generator

---
+++ Generators/ByteGenerator.cs
@@ -0,0 +1,18 @@
+﻿using Core.Context;
+using Core.Interfaces;
+
+namespace Core.Generators
+{
+    public class ByteGenerator : IValueGenerator
+    {
+        public bool CanGenerate(Type type)
+        {
+            return type == typeof(byte);
+        }
+
+        public object Generate(Type typeToGenerate, GeneratorContext context)
+        {
+            return context.Random.Next(1, byte.MaxValue);
+        }
+    }
+}
