24fc266c9ac548d9f8e3ca3cec0269e70e4f1072
KJI0YH <aliaksei.kryzhanouski@gmail.com>
2022-09-28 21:45:27 +0300

    add long generator

---
+++ Generators/LongGenerator.cs
@@ -0,0 +1,18 @@
+﻿using Core.Context;
+using Core.Interfaces;
+
+namespace Core.Generators
+{
+    public class LongGenerator : IValueGenerator
+    {
+        public bool CanGenerate(Type type)
+        {
+            return type == typeof(long);
+        }
+
+        public object Generate(Type typeToGenerate, GeneratorContext context)
+        {
+            return context.Random.NextInt64(1, long.MaxValue);
+        }
+    }
+}
