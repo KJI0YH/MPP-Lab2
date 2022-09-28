60517516267d09a220287d38f70c5f4b3008ad26
KJI0YH <aliaksei.kryzhanouski@gmail.com>
2022-09-28 22:17:30 +0300

    add char generator

---
+++ Generators/CharGenerator.cs
@@ -0,0 +1,21 @@
+﻿using Core.Context;
+using Core.Interfaces;
+
+namespace Core.Generators
+{
+    public class CharGenerator : IValueGenerator
+    {
+
+        private readonly char[] _chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+=-[]{}/\\'\"/|;:`~№<>.,".ToCharArray();
+
+        public bool CanGenerate(Type type)
+        {
+            return type == typeof(char);
+        }
+
+        public object Generate(Type typeToGenerate, GeneratorContext context)
+        {
+            return _chars[context.Random.Next(0, _chars.Length)];
+        }
+    }
+}
