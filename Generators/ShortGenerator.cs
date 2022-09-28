64b079650a402bb1d070d8e96d076a025994ed20
KJI0YH <aliaksei.kryzhanouski@gmail.com>
2022-09-28 22:19:50 +0300

    add short generator

---
+++ Generators/ShortGenerator.cs
@@ -0,0 +1,18 @@
+﻿using Core.Context;
+using Core.Interfaces;
+
+namespace Core.Generators
+{
+    public class ShortGenerator : IValueGenerator
+    {
+        public bool CanGenerate(Type type)
+        {
+            return type == typeof(short);
+        }
+
+        public object Generate(Type typeToGenerate, GeneratorContext context)
+        {
+            return context.Random.Next(1, short.MaxValue);
+        }
+    }
+}
