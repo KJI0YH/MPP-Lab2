212f14161dfbd2801340847f4bf832ef0ef4c878
KJI0YH <aliaksei.kryzhanouski@gmail.com>
2022-09-28 22:01:12 +0300

    add string generator

---
+++ Generators/StringGenerator.cs
@@ -0,0 +1,26 @@
+﻿using Core.Context;
+using Core.Interfaces;
+
+namespace Core.Generators
+{
+    public class StringGenerator : IValueGenerator
+    {
+        private readonly string _dict = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+=-[]{}/\\'\"/|;:`~№<>.,";
+
+        public bool CanGenerate(Type type)
+        {
+            return type == typeof(string);
+        }
+
+        public object Generate(Type typeToGenerate, GeneratorContext context)
+        {
+            int lenght = context.Random.Next(8, 100);
+            string result = string.Empty;
+            for (int i = 0; i < lenght; i++)
+            {
+                result += _dict[context.Random.Next(0, _dict.Length)];
+            }
+            return result;
+        }
+    }
+}
