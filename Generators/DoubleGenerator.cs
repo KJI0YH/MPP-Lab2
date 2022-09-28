aa2b53c0ca1a9d1b388a9d3fd06cd3f103ccb592
KJI0YH <aliaksei.kryzhanouski@gmail.com>
2022-09-28 21:47:30 +0300

    add double generator

---
+++ Generators/DoubleGenerator.cs
@@ -0,0 +1,18 @@
+﻿using Core.Context;
+using Core.Interfaces;
+
+namespace Core.Generators
+{
+    public class DoubleGenerator : IValueGenerator
+    {
+        public bool CanGenerate(Type type)
+        {
+            return type == typeof(double);
+        }
+
+        public object Generate(Type typeToGenerate, GeneratorContext context)
+        {
+            return context.Random.NextDouble();
+        }
+    }
+}
