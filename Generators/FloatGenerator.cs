861332116884254426e9e06a5c657a08c8ea2962
KJI0YH <aliaksei.kryzhanouski@gmail.com>
2022-09-28 21:49:04 +0300

    add float generator

---
+++ Generators/FloatGenerator.cs
@@ -0,0 +1,18 @@
+﻿using Core.Context;
+using Core.Interfaces;
+
+namespace Core.Generators
+{
+    public class FloatGenerator : IValueGenerator
+    {
+        public bool CanGenerate(Type type)
+        {
+            return type == typeof(float);
+        }
+
+        public object Generate(Type typeToGenerate, GeneratorContext context)
+        {
+            return context.Random.NextSingle();
+        }
+    }
+}
