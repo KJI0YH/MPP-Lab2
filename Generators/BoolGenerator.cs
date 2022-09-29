36bb9dfb90fafc21d4cc14d8bd5b3ec82b0b4d80
KJI0YH <aliaksei.kryzhanouski@gmail.com>
2022-09-29 21:35:14 +0300

    add bool generator

---
+++ Generators/BoolGenerator.cs
@@ -0,0 +1,23 @@
+﻿using Core.Context;
+using Core.Interfaces;
+
+namespace Core.Generators
+{
+    public class BoolGenerator : IValueGenerator
+    {
+        public bool CanGenerate(Type type)
+        {
+            return type == typeof(bool);
+        }
+
+        public object Generate(Type typeToGenerate, GeneratorContext context)
+        {
+            return true;
+        }
+
+        public Type GetGeneratedType()
+        {
+            return typeof(bool);
+        }
+    }
+}
